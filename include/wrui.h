9b4982cf69c5bade39e003a5961a4e8b9b06c4a2
Daniel Collin <daniel@collin.com>
2016-10-23 17:30:06 +0200

    WIP
    (breaks the build but will be fixed later)

--- include/wrui.h
+++ include/wrui.h
@@ -6,31 +6,44 @@ extern "C" {
 
 #define GU_EVENT_RELEASED "2released()"
 
+typedef struct GUObjectFuncs {
+	int (*connect)(struct GUWidget* widget, const char* id, void* reciver, void* func);
+} GUObjectFuncs;
+
+typedef struct GUWidgetFuncs {
+	void (*set_size)(struct GUWidget* widget, int width, int height);
+} GUWidgetFuncs;
+
+typedef struct GUPushButtonFuncs {
+	void (*set_default)(struct GUPushButton* button, int state);
+} GUPushButtonFuncs;
+
+typedef struct GUApplicationFuncs {
+	int (*run)(void* p);
+} GUApplicationFuncs;
+
 typedef struct GUObject {
-	// private data
 	void* p;
-	int (*connect)(void* sender, const char* id, void* reciver, void* func);
-	//int (*connect_event)(void* recvier, const char* id, void* reciver, void* func);
+	GUObjectFuncs* funcs;
 } GUObject;
 
 typedef struct GUWidget {
-	GUObject* o;
-	void (*set_size)(struct GUWidget* widget, int width, int height);
+	GUObject object;
+	GUWidgetFuncs* funcs;
 } GUWidget;
 
 typedef struct GUWindow {
-	GUWidget* base;
+	GUWidget base;
 } GUIWindow;
 
 typedef struct GUPushButton {
-	GUWidget* base;
-	void (*set_default)(struct GUPushButton* button, int state);
+	GUWidget base;
+	GUPushButtonFuncs* funcs;
 } GUPushButton;
 
 typedef struct GUApplication {
-	// private data
-	void* p;
-	int (*run)(void* p);
+	void* p; // private data
+	GUApplicationFuncs* funcs;
 } GUApplication;
 
 typedef struct Wrui {
@@ -41,13 +54,13 @@ typedef struct Wrui {
 
 // Windowing
 
-#define gu_create_window(funcs) funs->create_window()
+#define gu_window_create(funcs) funcs->create_window()
 
 // Generic window stuff
 
-#define gu_set_size(widget, x, y) widget->base->set_size(widget->base, x, y)
+#define gu_set_size(widget, x, y) widget->base.funcs->set_size(widget->base, x, y)
 #define gu_set_parent(widget, parent) widget->base->set_parent(widget->base, widget->base)
-#define gu_push_button_crate(funcs, label) funcs->push_button_create(label)
+#define gu_push_button_create(funcs, label) funcs->push_button_create(label)
 
 // Connection API
 
